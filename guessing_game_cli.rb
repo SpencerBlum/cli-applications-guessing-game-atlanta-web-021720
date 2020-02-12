9c9e7a4e241d23a2983d613ed766505623535b17
Spencer Blum <spencerblum@gmail.com>
2020-02-12 22:35:58 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,12 +5,12 @@ def run_guessing_game
 
   puts "Guess a number between 1 and 6:"
   get_number = gets.chomp
-  if random_num == get_number
+  if random_num.to_s == get_number
   puts "You guessed the correct number!"
 
   elsif get_number.downcase == "exit"
   puts "Goodbye!"
-  
+
   else
     puts "Sorry! The computer guessed #{random_num}."
   end
