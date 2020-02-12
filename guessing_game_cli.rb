1e030917f1db6152357f8a244e68bba164e71de2
Spencer Blum <spencerblum@gmail.com>
2020-02-12 22:20:09 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,4 +2,12 @@
 
 def run_guessing_game
   random_num = rand(1..6)
+  get_number = gets.chomp
+  if random_num == get_number
+  return "You guessed the correct number!"
+  elsif random_num != get_number
+  return "Sorry! The computer guessed #{random_num}"
+elsif get_number == "exit"
+  return Goodbye
+  end
 end
