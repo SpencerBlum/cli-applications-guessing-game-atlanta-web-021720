ebe19c9dc3e0b8897582944ce76dc0573ff1b098
Spencer Blum <spencerblum@gmail.com>
2020-02-12 22:34:30 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,9 +7,11 @@ def run_guessing_game
   get_number = gets.chomp
   if random_num == get_number
   puts "You guessed the correct number!"
-  elsif random_num != get_number
-  puts "Sorry! The computer guessed #{random_num}."
+  
   elsif get_number.downcase == "exit"
   puts "Goodbye!"
+  
+  else
+    puts "Sorry! The computer guessed #{random_num}."
   end
 end
